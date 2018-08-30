be4e3684484c32d589bbf389c16527a1a0f9d3ec
Nico Verzijp <nverzijp@gmail.com>
2018-08-30 11:22:57 +0200

    Implement

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Nico Verzijp on 29/08/2018.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# FoodTracker
+
+[Start Developing iOS Apps (Swift)](https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/)
+
